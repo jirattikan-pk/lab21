75dea87782424872b0b7f020c072ac7baf6b8051
jirattikan-pk <mindmook.2006@gmail.com>
2026-02-24 15:22:19 +0700

    cp1_21

--- average.cpp
+++ average.cpp
@@ -1,1 +1,16 @@
-
+#include <iostream>
+using namespace std;
+int main(int argc, char* argv[]){
+    double sum = 0;
+    if(argc <= 1){
+        cout << "Please input numbers to find average.";
+        return 1;
+    }
+    for(int i=1;i<argc;i++){
+        sum += atof(argv[i]);
+    }
+    cout << "---------------------------------\n";
+    cout << "Average of "<<argc-1 <<" numbers = " << sum/(argc-1) <<"\n";
+    cout << "---------------------------------";
+    return 0;
+}
